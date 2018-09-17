00c87419887bd47a22331a7ee17cda77100ac810
Hack Reactor Student <student@hackreactor.com>
2018-09-17 14:15:07 -0700

    Complete the getElementsByClassName with recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,18 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var result = [];
+  var classBody = document.body;
+  function grab (Body) {
+    if (Body.classList && Body.classList.contains(className)) {
+      result.push(Body);   
+    }
+    if (Body.childNodes) {
+    for (var i = 0; i < Body.childNodes.length; i++) {
+      grab(Body.childNodes[i]);
+    }
+    }
+  }
+  grab(classBody);
+  return result;
 };
