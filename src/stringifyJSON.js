713f8efdcd283f2bc1a0b8b224c60612a360adf8
Hack Reactor Student <student@hackreactor.com>
2018-09-17 11:50:52 -0700

    strings, booleans, arrays complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  var result = [];
+  
+  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null){
+    return String(obj);
+  }else if(Array.isArray(obj)){
+    if(obj[0] === undefined){
+      return '[]';
+    } else {
+      obj.forEach(v => {
+        result.push(stringifyJSON(v));
+      })
+      return '[' + result.join(',') + ']';
+    }
+  }else if(typeof obj === 'string'){
+    return '"' + obj + '"';
+  }else if(typeof obj === 'object'){
+    if(Object.keys(obj) === undefined){
+      return '{}'
+    }else{
+      for(let key in obj){
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    return '{' + result.join(',') + '}';
+  }
 };
