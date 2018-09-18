a5607a3d234143642623e7b706648faffafa7d9f
Hack Reactor Student <student@hackreactor.com>
2018-09-17 20:00:46 -0700

    Implement array parsing

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,54 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  let charArr = json.split('');
+  
+  function getNextChar(){
+    let nextChar = charArr.shift();
+    if(nextChar === ' ' || nextChar === ','){
+      return getNextChar();
+    }else{
+      return nextChar;
+    }
+  }
+
+  function array(elements){
+    // if(elements === undefined){
+    //   return [];
+    // }else{
+      return [elements];
+
+  }
+
+  function string(char){
+  }
+
+  function object(members){
+    
+  }
+
+  function number(char){
+    return Number(char);
+  }
+
+  function boolean(){}
+
+
+  function cases(nextChar){
+    if(nextChar === '['){
+      let arr = [];
+      for(let i = 0; i < charArr.indexOf(']'); i++){
+        arr.push(cases(getNextChar()));
+      }
+      return arr;
+    }else if(!isNaN(nextChar)){
+      return number(nextChar)
+    }
+  }
+ 
+  return cases(getNextChar());
+
+
+  
 };
